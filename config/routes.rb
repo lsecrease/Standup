3c556ed48c529f4e93da8fb7fac91a900c9aa12e
Lawrence Secrease <lawrence@bookshout.com>
2017-06-07 23:21:46 -0500

    Start of authentication

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  devise_for :users
+  get 'activity/mine'
+
+  get 'activity/feed'
+ 
+  root to: 'activity#mine'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
